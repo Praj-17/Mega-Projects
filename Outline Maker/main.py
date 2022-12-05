14b49dd82170a284c849bafc54f6b290b17fc78d
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2022-12-05 14:11:55 +0530

    presented the code to sir

--- Outline Maker/main.py
+++ Outline Maker/main.py
@@ -1,5 +1,5 @@
 import cv2
-from rembg import remove
+from rembg import remove #removes bg
 
 def main (image_path, output_name):
     image = cv2.imread(image_path)
@@ -12,19 +12,29 @@ def main (image_path, output_name):
     while True:
         img = image.copy()
         r = cv2.selectROI("select the area (Press 'Enter' to continue and 'c' to stop)", image)
+        print(r)
 
         # Crop image
         cropped_image = img[int(r[1]):int(r[1]+r[3]),
                             int(r[0]):int(r[0]+r[2])]
+        cv2.imshow( "Input Image",cropped_image)
+        cv2.waitKey(0)
+        
         cropped_wo_bg = remove( cropped_image)
+        cv2.imshow( "Input Image",cropped_wo_bg)
+        cv2.waitKey(0)
+                    
         gray= cv2.cvtColor(cropped_wo_bg, cv2.COLOR_BGR2GRAY)
+        
+        cv2.imshow( "Input Image",gray)
+        cv2.waitKey(0)
 
         edges= cv2.Canny(gray,30,200)
 
         contours, hierarchy= cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         cv2.drawContours(img[int(r[1]):int(r[1]+r[3]),
-            int(r[0]):int(r[0]+r[2])], contours, -1, (0,255,0),2, hierarchy= hierarchy)
+            int(r[0]):int(r[0]+r[2])], contours, -1, (255,0,0),2, hierarchy= hierarchy)
         
         cv2.imwrite("Results/" + output_name, img)
         cv2.imshow('Output Image',img)
